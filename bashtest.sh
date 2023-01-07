266e4031d39b7682051d8d6b1dc177d54558ec17
vijay <pvijaykumar831@gmail.com>
2023-01-07 20:28:28 +0530

    added a line

--- bashtest.sh
+++ bashtest.sh
@@ -6,3 +6,10 @@ echo -e "\e[32m mounted disks \e[0m"
 df -h
 echo -e "\e[32m logged in users \e[0m"
 w
+
+if [ -f /etc/fstab ]; then
+  echo -e "\e[32m fstab file is present \e[0m"
+  else
+    echo -e "\e[32m fstab file is need to be created \e[0m"
+
+fi
